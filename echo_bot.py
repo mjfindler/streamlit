79015eb3958d9077f2f3eada48aaa607493187d4
mjfindler <156967314+mjfindler@users.noreply.github.com>
2026-06-03 14:19:30 -0400

    Update echo_bot.py

--- echo_bot.py
+++ echo_bot.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 
-# with st.chat_message("assistant"):
-with st.chat_message("user"):
+with st.chat_message("assistant"):
+# with st.chat_message("user"):
     st.write("Hello human")
     st.bar_chart(np.random.randn(30, 3))
